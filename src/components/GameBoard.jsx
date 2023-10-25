068159a723c46e95cb36c53930bfb79b9a0f9aac
Divum <rkeerthick1@gmail.com>
2023-10-25 14:27:33 +0530

    commit

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,22 +1,6 @@
 import React, { useState } from "react";
 
-
 const GameBoard = ({ handleSelectSquare, board }) => {
-
-  
-
-
-  // const [gameBoard, setGameBoard] = useState(board);
-  // const handleClick = (rowIndex, colIndex, symbol) => {
-  //   setGameBoard((prevState) => {
-  //     let updatedGameBoard = [
-  //       ...prevState.map((innerArray) => [...innerArray]),
-  //     ];
-  //     updatedGameBoard[rowIndex][colIndex] = activePlayer;
-  //     return updatedGameBoard;
-  //   });
-  //   handlePlayer();
-  // };
   return (
     <ol id="game-board">
       {board.map((row, rowIndex) => {
@@ -26,7 +10,12 @@ const GameBoard = ({ handleSelectSquare, board }) => {
               {row.map((playerSymbol, colIndex) => {
                 return (
                   <li key={colIndex}>
-                    <button disabled={playerSymbol !== null ? true : false} onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                    <button
+                      disabled={playerSymbol !== null ? true : false}
+                      onClick={() => handleSelectSquare(rowIndex, colIndex)}
+                    >
+                      {playerSymbol}
+                    </button>
                   </li>
                 );
               })}
